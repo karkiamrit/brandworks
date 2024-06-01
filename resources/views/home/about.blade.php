40fbd2434ecc29f330cb9a836c70fc8b9ce77fda
karkiamrit <amrit.karki2073@gmail.com>
2024-06-01 23:35:33 +0545

    about page added

--- resources/views/home/about.blade.php
+++ resources/views/home/about.blade.php
@@ -1,5 +1,100 @@
 <x-guest-layout>
+    <div class="min-h-screen lg:px-[110px] pb-14 ">
 
-    <div class="">
+        <div class="mx-auto items-center space-y-4  justify-center flex flex-col  h-[90vh] py-16">
+            <div class="w-fit">
+                <h2 class="text-4xl text-start">Why</h2>
+                <img src="{{ asset('eventAsset/image19.png') }}" alt="Logo" class="w-fit">
+            </div>
+        </div>
+
+        <div class="grid grid-cols-8 relative  bg-[#707070] py-16 ">
+            <div class="absolute top-[10%] right-0 w-fit text-center left-13 px-12 leading-[50px]">
+                <div class="">
+                    <h1 class="text-5xl !leading-[120px] text-white">PAN Nepal Reach</h1>
+                    <p class="text-3xl text-white">with regional workforce available 24 x 7</p>
+                </div>
+                =
+            </div>
+            <div class=" w-full space-y-12 self-end ms-24 col-span-2 ">
+                <div class="flex space-x-6 mx-4">
+                    <img src="{{ asset('eventAsset/image17.png') }}" class="w-[25px] h-[28px] text-center self-center"
+                         alt="">
+                    <p class="text-red-700  text-2xl">12 QFX network offices</p>
+                </div>
+
+                <div class="flex space-x-6 mx-4">
+                    <img src="{{ asset('eventAsset/image17.png') }}" class="w-[25px] h-[28px] text-center self-center"
+                         alt="">
+                    <p class="text-green-500  text-2xl">12 QFX network offices</p>
+                </div>
+
+                <div class="flex space-x-6 mx-4">
+                    <img src="{{ asset('eventAsset/image17.png') }}" class="w-[25px] h-[28px] text-center self-center"
+                         alt="">
+                    <p class="text-yellow-400  text-2xl">12 QFX network offices</p>
+                </div>
+            </div>
+            <div class="w-full self-end col-span-6">
+                <img src="{{ asset('eventAsset/nepal-map.png') }}" alt="Logo" class="w-full">
+            </div>
+        </div>
+
+
+        <div class="grid grid-cols-8 py-4  pt-20git">
+            <div class="col-span-2 py-16 px-4 bg-white relative">
+                <h2 class="text-4xl text-start">Why</h2>
+                <img src="{{ asset('eventAsset/image19.png') }}" alt="Logo" class="w-[65%]">
+                <div class="absolute left-[15%] top-1/3 space-y-12">
+                    <div class="flex items-center justify-end space-x-3">
+                        <p class="text-6xl font-bold text-blue-600">21+Years</p>
+                        <p class="text-2xl text-gray-800 !leading-[1.2rem] pt-3">Collective experience</p>
+                    </div>
+                    <div class="flex items-center justify-end space-x-3">
+                        <p class="text-6xl font-bold text-blue-600">991+</p>
+                        <p class="text-2xl text-gray-800 !leading-[1.2rem] pt-3">Projects</p>
+                    </div>
+                    <div class="flex items-center justify-end space-x-3">
+                        <p class="text-6xl font-bold text-blue-600">31+</p>
+                        <p class="text-2xl text-gray-800 !leading-[1.2rem] pt-3">Clients</p>
+                    </div>
+
+                    <div class="flex items-center justify-end space-x-3">
+                        <p class="text-6xl font-bold text-blue-600">11+</p>
+                        <p class="text-2xl text-gray-800 !leading-[1.2rem] pt-3 w-1/5">Seasoned
+                            professionals
+                            from
+                            difference
+                            industries</p>
+                    </div>
+
+                </div>
+            </div>
+            <div class="col-span-6">
+                <img src="{{ asset('eventAsset/image18.png') }}" alt="Logo" class="w-full">
+            </div>
+        </div>
+
+        <div class="grid grid-cols-6 gap-8 pt-20">
+            <div class="grid col-span-3 h-2/3 px-10">
+                <img src="{{ asset('eventAsset/image19.jpg') }}" alt="Logo" class="h-full w-full ">
+                <p class="text-3xl">We are proud to have worked with trusted
+                    international and home-grown companies.</p>
+            </div>
+
+            <div class="grid grid-cols-4 col-span-3 gap-5">
+                @for ($i =1 ; $i <= 35; $i++)
+                    <div class="flex items-center justify-center w-32 h-32 "> <!-- Fixed width and height -->
+                        <img src="{{ asset('eventAsset/client-logos/' . $i . '.png') }}" alt="{{$i}}" class="h-full w-full mix-blend-multiply object-contain"> <!-- Ensure images fit within the container -->
+                    </div>
+                @endfor
+
+
+
+
+            </div>
+
+        </div>
+
     </div>
 </x-guest-layout>
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,80 +1,109 @@
 <x-guest-layout>
-        <!-- Logo and Center Content -->
+    <!-- Logo and Center Content -->
     <div class="mx-12">
 
-            <div class="h-[90vh]  flex  flex-col justify-center ">
-                <div class="text-center ">
-                    <img src="{{ asset('home/logo.png') }}" alt="Logo" class="mx-auto">
+        <div class="h-[90vh]  flex  flex-col justify-center ">
+            <div class="text-center ">
+                <img src="{{ asset('home/logo.png') }}" alt="Logo" class="mx-auto">
+            </div>
+            <p class="text-center md:text-2xl text-xl flex flex-col">
+                <span>Brand actions, Events,</span>
+                <span>Activation, Fabrication </span>
+            </p>
+        </div>
+
+        <div class="space-y-12 mb-40 lg:px-[110px]">
+            <div class="text-center ">
+                <img src="{{ asset('home/avani-logo.png') }}" alt="Logo" class="mx-auto">
+            </div>
+
+            <div class="grid lg:grid-cols-6 grid-cols-1 md:gap-4 align-middle min-h-fit gap-6 ">
+                <div class="text-center col-span-1">
+                    <div>
+                        <img src="{{ asset('home/avani-logo.png') }}" alt="Logo"
+                             class="mx-auto md:w-2/3 w-1/2 h-[120px]">
+                    </div>
+                    <p class="md:text-xl text-sm text-gray-500">Advertising, Branding, Communication</p>
                 </div>
-                <p class="text-center md:text-2xl text-xl flex flex-col">
-                  <span>Brand actions, Events,</span>
-                    <span>Activation, Fabrication </span>
+                <div class="text-center col-span-1">
+                    <div>
+                        <img src="{{ asset('home/logo.png') }}" alt="Logo" class="mx-auto md:w-2/3 w-1/2 h-[120px]">
+                    </div>
+                    <p class="md:text-xl text-sm text-gray-500">Brand actions, Events,
+                        Activation, Fabrication</p>
+                </div>
+                <div class="text-center col-span-1">
+                    <div>
+                        <img src="{{ asset('home/limez-marketing.png') }}" alt="Logo"
+                             class="mx-auto md:w-2/3 w-1/2 h-[120px]">
+                    </div>
+                    <p class="md:text-xl text-sm text-gray-500">Marketing Partner for QFX,
+                        DCN and LABIM Mall</p>
+                </div>
+                <div class="text-center col-span-1">
+                    <div>
+                        <img src="{{ asset('home/marching-ant.png') }}" alt="Logo"
+                             class="mx-auto md:w-2/3 w-1/2 h-[120px]">
+                    </div>
+                    <p class="md:text-xl text-sm text-gray-500">Data-driven Digital
+                        Marketing</p>
+                </div>
+                <div class="text-center col-span-1">
+                    <div>
+                        <img src="{{ asset('home/mantra-it.png') }}" alt="Logo"
+                             class="mx-auto md:w-2/3 w-1/2 h-[120px]">
+                    </div>
+                    <p class="md:text-xl text-sm text-gray-500">IT Services</p>
+                </div>
+                <div class="text-center col-span-1">
+                    <div>
+                        <img src="{{ asset('home/impact.png') }}" alt="Logo" class="mx-auto md:w-2/3 w-1/2 h-[120px]">
+                    </div>
+                    <p class="md:text-xl text-sm text-gray-500">Creative Marketing
+                        and Communications</p>
+                </div>
+            </div>
+
+            <div
+                class="py-3 flex items-center text-3xl text-gray-400  before:flex-1 before:border-t before:border-gray-500 before:me-6 after:flex-1 after:border-t after:border-gray-500 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">
+                avani aligns the best of the best
+            </div>
+
+            <div class="md:flex items-center sm:flex-wrap md:flex-nowrap">
+                <div>
+                    <img src="{{ asset('home/logo.png') }}" alt="Logo"
+                         class="mx-auto md:w-3/4 md:border-r md:border-b-0 border-b border-gray-500">
+                </div>
+                <p class="md:text-xl lg:text-3xl text-xl text-center md:text-start  leading-10  md:w-[75%]">
+                    started with a vision to support businesses with brand establishment, quality branding,
+                    and creative design services; brandworks, is now a well-established independent firm
+                    specializing in events, fabrications, brand actions and purpose-driven engagements.
                 </p>
             </div>
 
-            <div class="space-y-12 mb-40">
-                <div class="text-center ">
-                    <img src="{{ asset('home/avani-logo.png') }}" alt="Logo" class="mx-auto">
+
+            <div class="bg-gray-200 grid lg:grid-cols-3 items-center justify-center  space-y-12 md:space-y-0 py-20 ">
+                <div class=" lg:col-span-1  ">
+                    <img src="{{ asset('eventAsset/image11.png') }}" alt="Logo" class="w-full">
                 </div>
-
-                <div class="grid lg:grid-cols-6 grid-cols-1 md:gap-4 align-middle min-h-fit gap-6 ">
-                    <div class="text-center col-span-1">
-                        <div>
-                            <img src="{{ asset('home/avani-logo.png') }}" alt="Logo" class="mx-auto md:w-2/3 w-1/2 h-[120px]">
-                        </div>
-                        <p class="md:text-xl text-sm text-gray-500">Advertising, Branding, Communication</p>
-                    </div>
-                    <div class="text-center col-span-1">
-                        <div>
-                            <img src="{{ asset('home/logo.png') }}" alt="Logo" class="mx-auto md:w-2/3 w-1/2 h-[120px]">
-                        </div>
-                        <p class="md:text-xl text-sm text-gray-500">Brand actions, Events,
-                            Activation, Fabrication</p>
-                    </div>
-                    <div class="text-center col-span-1">
-                        <div>
-                            <img src="{{ asset('home/limez-marketing.png') }}" alt="Logo" class="mx-auto md:w-2/3 w-1/2 h-[120px]">
-                        </div>
-                        <p class="md:text-xl text-sm text-gray-500">Marketing Partner for QFX,
-                            DCN and LABIM Mall</p>
-                    </div>
-                    <div class="text-center col-span-1">
-                        <div>
-                            <img src="{{ asset('home/marching-ant.png') }}" alt="Logo" class="mx-auto md:w-2/3 w-1/2 h-[120px]">
-                        </div>
-                        <p class="md:text-xl text-sm text-gray-500">Data-driven Digital
-                            Marketing</p>
-                    </div>
-                    <div class="text-center col-span-1">
-                        <div>
-                            <img src="{{ asset('home/mantra-it.png') }}" alt="Logo" class="mx-auto md:w-2/3 w-1/2 h-[120px]">
-                        </div>
-                        <p class="md:text-xl text-sm text-gray-500">IT Services</p>
-                    </div>
-                    <div class="text-center col-span-1">
-                        <div>
-                            <img src="{{ asset('home/impact.png') }}" alt="Logo" class="mx-auto md:w-2/3 w-1/2 h-[120px]">
-                        </div>
-                        <p class="md:text-xl text-sm text-gray-500">Creative Marketing
-                            and Communications</p>
-                    </div>
+                <div class="lg:col-span-2  lg:w-2/3 mx-auto bg-gradient-to-bl  self-center justify-center ">
+                    <p class="md:text-xl lg:text-3xl text-xl lg:!leading-[3.2rem]">a motley group of strategic creative,
+                        design, and marketing professionals who
+                        believe in action to change the way
+                        businesses communicate to the world.</p>
                 </div>
-
-                <div class="py-3 flex items-center text-3xl text-gray-400  before:flex-1 before:border-t before:border-gray-500 before:me-6 after:flex-1 after:border-t after:border-gray-500 after:ms-6 dark:text-neutral-500 dark:before:border-neutral-600 dark:after:border-neutral-600">avani aligns the best of the best</div>
-
-                <div class="flex  items-center sm:flex-wrap md:flex-nowrap">
-                    <div>
-                        <img src="{{ asset('home/logo.png') }}" alt="Logo" class="mx-auto md:w-3/4 md:border-r md:border-b-0 border-b border-gray-500">
-                    </div>
-                    <p class="md:text-xl lg:text-3xl text-xl text-center md:text-start  leading-10  md:w-[75%]">
-                        started with a vision to support businesses with brand establishment, quality branding,
-                        and creative design services; brandworks, is now a well-established independent firm
-                        specializing in events, fabrications, brand actions and purpose-driven engagements.
-                    </p>
-                </div>
-
-
             </div>
+
+
+            <div class=" lg:relative space-y-12 md:space-y-0 h-[60%]  ">
+                <div class="bg-gray-300  md:w-1/2">
+                    <img src="{{ asset('eventAsset/image13.png') }}" alt="Logo" class="w-full">
+                </div>
+                <p class="lg:absolute lg:top-[30%] lg:w-[650px]  lg:left-[46%] lg:!leading-[3rem] lg:text-3xl text-xl "> Our  focus  is  on  collaborating  with  visionary  leaders and  organizations  who  share  our  belief  that the  power  of  brand  actions,  design  and  innovation  can  have  a  profound  impact  shaping  brighter  future  for  the  community  they  serve. </p>
+            </div>
+
+
+        </div>
 
     </div>
 
